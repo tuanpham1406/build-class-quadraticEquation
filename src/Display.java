122784acdfba16334c94c75a7abb484c880ba3e6
tuanpham1406 <phamanhtuan1406@gmail.com>
2019-05-07 18:04:46 +0700

    [Bài tập] Xây dựng lớp QuadraticEquation

--- src/Display.java
+++ src/Display.java
@@ -12,9 +12,9 @@ public class Display {
     QuadraticEquation quadraticEquation = new QuadraticEquation(a, b, c);
     double delta = quadraticEquation.getDiscriminant();
     if (delta >= 0) {
-      System.out.println("Equation 1 = " + quadraticEquation.getRoot1() + " Equation 2 = " + quadraticEquation.getRoot2());
+      System.out.println("Root 1 = " + quadraticEquation.getRoot1() + " Root 2 = " + quadraticEquation.getRoot2());
     } else if (delta == 0) {
-      System.out.println("Equation = " + quadraticEquation.getRoot1());
+      System.out.println("Root = " + quadraticEquation.getRoot1());
     } else {
       System.out.println("The equation has no roots");
     }
--- src/QuadraticEquation.java
+++ src/QuadraticEquation.java
@@ -2,19 +2,32 @@ public class QuadraticEquation {
   private double a, b, c, delta, r1, r2;
   public QuadraticEquation() {
   }
+
+  public double getA() {
+    return a;
+  }
+
+  public double getB() {
+    return b;
+  }
+
+  public double getC() {
+    return c;
+  }
+
   public QuadraticEquation(double a, double b, double c) {
     this.a = a;
     this.b = b;
     this.c = c;
   }
   public double getDiscriminant() {
-    return delta = this.b*this.b - 4*this.a*this.c;
+    return delta = b*b - 4*a*c;
 
   }
   public double getRoot1() {
-    return r1 = (-this.b + Math.pow(delta, 0.5))/(2*this.a);
+    return r1 = (-b + Math.pow(delta, 0.5))/(2*a);
   }
   public double getRoot2() {
-    return r2 = (-this.b - Math.pow(delta, 0.5))/(2*this.a);
+    return r2 = (-b - Math.pow(delta, 0.5))/(2*a);
   }
 }
